c6ceb91e32fa423a07285c791f41647e6881be80
sphilmoon <sphilmoon@gmail.com>
2020-12-23 20:30:23 +0900

    added people list as a sample data.

--- django_web/blog/views.py
+++ django_web/blog/views.py
@@ -1,13 +1,29 @@
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-
-from django.http import HttpResponse
-
 # use HOME function using 'request' arg.
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
-
+	context = {
+		'people': people
+	}
+	return render(request, 'blog_temp/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog_temp/about.html', {'title': 'About'})
+
+# dummy data.
+people = {'Name': 'Ford Prefect',
+'Gender': 'Male',
+'Occupation': 'Researcher', 
+'Home_Planet': 'Betelgeuse Seven' },
+{'Gender': 'Female', 
+'Occupation': 'Mathematician', 
+'Home Planet': 'Earth' },
+{'Occupation': 'Sandwich-Maker', 
+'Gender': 'Male', 'Home Planet': 'Earth', 
+'Name': 'Arthur'}
+
+
+# def about(request):
+#	return HttpResponse('<h1>Blog About</h1>')
